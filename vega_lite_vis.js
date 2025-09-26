8069dd23787bd22896bc9495c8e3f7dcac617f1c
YeeXuanNg <161012806+YeeXuanNg@users.noreply.github.com>
2025-09-27 02:05:53 +0800

    Upload vega_lite_vis.js

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "malaysia_domestic_water_consumption.vg.json";
+vegaEmbed("#malaysia_domestic_water_consumption", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as resut.view
+}).catch(console.error);
